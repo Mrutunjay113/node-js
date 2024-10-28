7c0a3118b984a3fc22e62676f86c40f71d72d90f
Mrutunjay Yadav <42071203+Mrutunjay113@users.noreply.github.com>
2024-10-28 20:57:21 +0530

    callback-hell

---
+++ Async-await/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const superagent = require("superagent");
+
+// Callback hell
+
+fs.readFile(`${__dirname}/dog.txt`, "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      if (err) return console.log(err.message);
+      console.log(res.body.message);
+      fs.writeFile("dog-img.txt", res.body.message, (err) => {
+        if (err) return console.log(err.message);
+        console.log("Random dog image saved to file!");
+      });
+    });
+});
