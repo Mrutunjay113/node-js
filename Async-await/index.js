e0c0ae227160eb9f4d28d3f62ee2c30bfd25b555
Mrutunjay Yadav <42071203+Mrutunjay113@users.noreply.github.com>
2024-10-28 21:17:14 +0530

    Refactor file reading and saving logic to use promises

--- Async-await/index.js
+++ Async-await/index.js
@@ -1,6 +1,15 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("I could not find that file");
+      resolve(data);
+    });
+  });
+};
+
 // Callback hell
 
 fs.readFile(`${__dirname}/dog.txt`, "utf8", (err, data) => {
@@ -19,3 +28,29 @@ fs.readFile(`${__dirname}/dog.txt`, "utf8", (err, data) => {
       });
     });
 });
+
+// Promises - then() method
+
+writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) =>
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("I could not write the file");
+      resolve("success");
+    })
+  );
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    return writeFilePro("dog-img.txt", res.body.message);
+  })
+  .then(() => {
+    console.log("Random dog image saved to file!");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
